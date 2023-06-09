f77ac1db7f54442d7ea8105282ef0a61e4299a60
Fred Eugine Adomako <vimportantemails@gmail.com>
2023-06-09 16:43:43 +0200

    .

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {Hompage, Navbar} from "./Hompage";
+import {LeaderboardPage} from "./Leaderboard Page";
+import {IFOPage} from "./IFO Page";
+import {SwapPage} from "./Swap Page";
+import {AffiliatesPage} from "./Affiliates Page";
+import React, { useState, useEffect } from 'react';
+import { useMediaQuery } from 'react-responsive'
+
+
+
+function App() {
+
+  return (
+      // place your page inside this div like  <div><Hompage/></div> or <div><Hompage></Hompage></div>
+      <div>
+          <Hompage></Hompage>
+      </div>
+
+  )
+}
+
+export default App;
